df2272e0a84c00a47d44fd2e373ce0664a28f444
Paulus Schoutsen <balloob@gmail.com>
2020-01-21 04:32:50 -0800

    Test that services registered (#41)

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,16 +1,24 @@
 """Test integration initialization."""
 
-from custom_components.zwave_mqtt import DOMAIN, PLATFORMS
+from custom_components.zwave_mqtt import DOMAIN, PLATFORMS, const
 
 from tests.common import setup_zwave
 
 
 async def test_init_entry(hass):
     """Test setting up config entry."""
-    await setup_zwave(hass)
+    await setup_zwave(hass, "generic_network_dump.csv")
 
-    # Verify everything loaded.
+    # Verify integration + platform loaded.
     assert "zwave_mqtt" in hass.config.components
     for platform in PLATFORMS:
         assert platform in hass.config.components, platform
         assert f"{platform}.{DOMAIN}" in hass.config.components, f"{platform}.{DOMAIN}"
+
+    # Verify services registered
+    assert hass.services.has_service(DOMAIN, const.SERVICE_ADD_NODE)
+    assert hass.services.has_service(DOMAIN, const.SERVICE_REMOVE_NODE)
+    assert hass.services.has_service(DOMAIN, const.SERVICE_REMOVE_FAILED_NODE)
+    assert hass.services.has_service(DOMAIN, const.SERVICE_REPLACE_FAILED_NODE)
+    assert hass.services.has_service(DOMAIN, const.SERVICE_CANCEL_COMMAND)
+    assert hass.services.has_service(DOMAIN, const.SERVICE_SET_CONFIG_PARAMETER)
