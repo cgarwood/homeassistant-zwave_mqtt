50f9fd7231e0ae5a7654b228ce4937288a8d3f41
Charles Garwood <cgarwood@gmail.com>
2020-03-20 17:01:03 -0400

    bump version to 0.0.6

--- custom_components/zwave_mqtt/const.py
+++ custom_components/zwave_mqtt/const.py
@@ -1,6 +1,6 @@
 """Constants for the zwave_mqtt integration."""
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 DOMAIN = "zwave_mqtt"
 PLATFORMS = ["binary_sensor", "fan", "sensor", "switch", "light"]
